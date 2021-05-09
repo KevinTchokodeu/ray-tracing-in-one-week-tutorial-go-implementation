c570416b5aa173df44e5398c154e4b667df148cb
Kevin Tchokodeu <kevinnsieyanji@yahoo.com>
2021-05-09 12:42:28 +0200

    6 : Antialiasing

--- pkg/trace/list.go
+++ pkg/trace/list.go
@@ -4,23 +4,23 @@ import "ray-tracing-in-one-week-tutorial-go-implementation/pkg/geom"
 
 // List of Surfaces
 type List struct {
-	SS []Surface
+	HH []Hittable
 }
 
 // Creates a new list of Surfaces
-func NewList(s ...Surface) List {
-	return List{SS: s}
+func NewList(s ...Hittable) List {
+	return List{HH: s}
 }
 
 // Finds the first intersection (if any) between Ray r and any of the Surfaces in the List.
 // If no intersection is found, t = 0.
 func (l List) Hit(r geom.Ray, tMin, tMax float64) (t float64, p geom.Vec3, n geom.Unit) {
 	closest := tMax
-	for _, s := range l.SS {
-		if st, sp, sn := s.Hit(r, tMin, closest); st > 0 {
-			closest, t = st, st
-			p = sp
-			n = sn
+	for _, h := range l.HH {
+		if ht, hp, hn := h.Hit(r, tMin, closest); ht > 0 {
+			closest, t = ht, ht
+			p = hp
+			n = hn
 		}
 	}
 	return
